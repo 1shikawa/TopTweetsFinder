1486bd52178750cb9d45b5d256ac742f871f2347
1shikawa <ishikawa.toru@gmail.com>
2018-11-16 21:05:07 +0900

    herokuup

--- TopTweetsFinderApp/urls.py
+++ TopTweetsFinderApp/urls.py
@@ -6,6 +6,6 @@ app_name = 'TopTweetsFinder'
 
 urlpatterns = [
     path(
-        '/index', views.TwitterFinderIndex.as_view(), name='TwitterFinderIndex'
+        'index/', views.TwitterFinderIndex.as_view(), name='TwitterFinderIndex'
     ),
     ]
