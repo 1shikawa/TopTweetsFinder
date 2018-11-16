5ab977b2bf0cec3b4eee10b56d458c5962f2f02a
1shikawa <ishikawa.toru@gmail.com>
2018-11-16 20:13:46 +0900

    20181116 herokuSetting

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn TopTweetsFinder.wsgi
--- TopTweetsFinder/settings.py
+++ TopTweetsFinder/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -309,3 +310,5 @@ if DEBUG:
     DEBUG_TOOLBAR_CONFIG = {
         'INTERCEPT_REDIRECTS': False,
     }
+
+django_heroku.settings(locals())
