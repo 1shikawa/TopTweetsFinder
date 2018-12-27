2bd616e9b78a08641af6330d73bc776c35f32e0f
1shikawa <ishikawa.toru@gmail.com>
2018-12-27 15:14:10 +0900

    20181227 settings.py modify

--- TopTweetsFinder/settings.py
+++ TopTweetsFinder/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cc^9(lfy_tntc6i2-y#!ff%6vkbao*c8w(3%(dmsnyjs3=v!n3'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
